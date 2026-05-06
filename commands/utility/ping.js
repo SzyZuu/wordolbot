87a52098de881cb4d1bcdf62e06315c37b39e569
SzyZu <skwarek116@gmail.com>
2026-05-06 19:00:15 +0200

    feat: cooldown

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
+    cooldown: 5,
     data: new SlashCommandBuilder().setName('ping').setDescription('PIIIING'),
     async execute(interaction) {
         await interaction.reply('POOOOONG');
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ client.once(Events.ClientReady, (readyClient) => {
 });
 
 client.commands = new Collection();
+client.cooldowns = new Collection();
 
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
@@ -28,31 +29,17 @@ for(const folder of commandFolders){
     }
 }
 
-client.on(Events.InteractionCreate, async (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
-    const command = interaction.client.commands.get(interaction.commandName);
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
 
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} >:0000`);
-        return;
+for (const file of eventFiles){
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once){
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event .execute(...args));
     }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if(interaction.replied || interaction.deferred){
-            await interaction.followUp({
-                content: 'Sum error whoopsie daisy',
-                flags: MessageFlags.Ephemeral,
-            });
-        } else {
-            await interaction.reply({
-                content: 'Whoopsie daisy yoohoo (error)',
-                flags: MessageFlags.Ephemeral,
-            });
-        }
-    }
-});
+}
 
 client.login(process.env.DISCORD_TOKEN);
