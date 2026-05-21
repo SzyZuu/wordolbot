b2b2cbe8d822d5bca6b6ab52c6a9c1856e20d650
SzyZu <skwarek116@gmail.com>
2026-05-21 17:13:42 +0200

    feat: base db wrapper

---
+++ db/index.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg');
+
+const pool = new Pool();
+
+module.exports = {
+	query: async (text, params) => {
+		const res = await pool.query(text, params);
+		return res;
+	},
+};
