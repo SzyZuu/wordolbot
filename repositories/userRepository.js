4437947740a7b1df40f23e5741d1c90e2d2f6ebb
SzyZu <skwarek116@gmail.com>
2026-05-25 18:59:12 +0200

    chore: add streaks as parameter for updating users

--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -1,25 +1,25 @@
 const db = require('../db');
 
-async function updateUser(userIds, names, guesses, gameNr) {
+async function updateUser(userIds, names, streaks, guesses, gameNr) {
 	const query = `
 	INSERT INTO users (user_id, name, streak, played_yesterday, avg_guesses, games_played, last_played)
 	SELECT 
 		id, 
 		user_name, 
-		1,
+		p_streak,
 		TRUE,
 		guess,
 		1,
-		$4
-	FROM UNNEST($1::bigint[], $2::text[], $3::numeric[]) AS t(id, user_name, guess)
+		$5
+	FROM UNNEST($1::bigint[], $2::text[], $3::numeric[], $4::numeric[]) AS t(id, user_name, p_streak, guess)
 	ON CONFLICT (user_id) DO UPDATE SET
 		streak           = users.streak + 1,
 		played_yesterday = TRUE,
 		avg_guesses      = ROUND((COALESCE(users.avg_guesses, 0) * users.games_played + EXCLUDED.avg_guesses) / (users.games_played + 1), 1),
 		games_played     = users.games_played + 1,
-		last_played		 = $4;
+		last_played		 = $5;
 	`;
-	await db.query(query, [userIds, names, guesses, gameNr]);
+	await db.query(query, [userIds, names, streaks, guesses, gameNr]);
 }
 
 async function getLazyUsers() {
