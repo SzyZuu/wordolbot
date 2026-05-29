a7d22b549f2f5480a5ba1f90a8bc199ff18381c6
SzyZu <skwarek116@gmail.com>
2026-05-29 20:10:13 +0200

    chore: small db cleanup

--- db/index.js
+++ db/index.js
@@ -1,31 +1,14 @@
 const { Pool } = require('pg');
+const pg = require('pg');
 
 const pool = new Pool();
+pg.types.setTypeParser(1083, function(stringValue) {
+	return stringValue;
+});
 
 module.exports = {
 	query: async (text, params) => {
 		const res = await pool.query(text, params);
 		return res;
-	},
-	init_db: async () => {
-		await pool.query(`
-		CREATE TABLE IF NOT EXISTS users (
-		  user_id BIGINT PRIMARY KEY,
-		  name varchar(255),
-		  streak INT,
-		  played_yesterday BOOL,
-		  avg_guesses DECIMAL(2, 1)
-		);
-		
-		CREATE TABLE IF NOT EXISTS servers (
-		  server_id BIGINT PRIMARY KEY
-		);
-		
-		CREATE TABLE IF NOT EXISTS user_servers (
-		  user_id BIGINT REFERENCES users(user_id) ON DELETE CASCADE,
-		  server_id BIGINT REFERENCES servers(server_id) ON DELETE CASCADE,
-		  PRIMARY KEY (user_id, server_id)
-		);
-		`);
 	},
 };
--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -2,19 +2,17 @@ const db = require('../db');
 
 async function updateUser(userIds, names, streaks, guesses, gameNr) {
 	const query = `
-	INSERT INTO users (user_id, name, streak, played_yesterday, avg_guesses, games_played, last_played)
+	INSERT INTO users (user_id, name, streak, avg_guesses, games_played, last_played)
 	SELECT 
 		id, 
 		user_name, 
 		p_streak,
-		TRUE,
 		guess,
 		1,
 		$5
 	FROM UNNEST($1::bigint[], $2::text[], $3::numeric[], $4::numeric[]) AS t(id, user_name, p_streak, guess)
 	ON CONFLICT (user_id) DO UPDATE SET
 		streak           = users.streak + 1,
-		played_yesterday = TRUE,
 		avg_guesses      = ROUND((COALESCE(users.avg_guesses, 0) * users.games_played + EXCLUDED.avg_guesses) / (users.games_played + 1), 1),
 		games_played     = users.games_played + 1,
 		last_played		 = $5;
