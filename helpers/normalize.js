bbe866a609b7c79dbf35523f0062081d6a7bcc3a
SzyZu <skwarek116@gmail.com>
2026-06-10 21:21:11 +0200

    refactor: export normalize because needed also in other stuffs

---
+++ helpers/normalize.js
@@ -0,0 +1,10 @@
+function normalize(str) {
+	return str
+		.toLowerCase()
+		.normalize('NFKD')
+		.replace(/[^\p{L}\p{N} ]/gu, '')
+		.replace(/\s+/g, ' ')
+		.trim();
+}
+
+module.exports = normalize;
