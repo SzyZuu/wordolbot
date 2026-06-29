4423abf2f0a6d5b2565923b86e5d118069369a3a
SzyZu <skwarek116@gmail.com>
2026-06-29 23:01:56 +0200

    fix: make shiz transaction because it has to be transaction

--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -1,37 +1,52 @@
 const db = require('../db');
 
 async function initializeUsers(userIds, names, streaks, guesses, totalGames, gameNr, serverID) {
-	const query = `
-	INSERT INTO users (user_id, name, streak, avg_guesses, games_played, last_played)
-	SELECT 
-		id, 
-		user_name, 
-		p_streak,
-		guess,
-		games,
-		$6
-	FROM UNNEST($1::bigint[], $2::text[], $3::numeric[], $4::numeric[], $5::numeric[]) AS t(id, user_name, p_streak, guess, games)
-	ON CONFLICT (user_id) DO NOTHING
-	`;
-	await db.query(query, [userIds, names, streaks, guesses, totalGames, gameNr]);
+	const client = await db.getClient();
+	try {
+		await client.query('BEGIN;');
 
-	const serverQuery = `
-	INSERT INTO servers (server_id, initialized) VALUES($1, TRUE)
-	ON CONFLICT (server_id) DO UPDATE SET initialized = TRUE
-	`;
+		const query = `
+		INSERT INTO users (user_id, name, streak, avg_guesses, games_played, last_played)
+		SELECT 
+			id, 
+			user_name, 
+			p_streak,
+			guess,
+			games,
+			$6
+		FROM UNNEST($1::bigint[], $2::text[], $3::numeric[], $4::numeric[], $5::numeric[]) AS t(id, user_name, p_streak, guess, games)
+		ON CONFLICT (user_id) DO NOTHING
+		`;
+		await client.query(query, [userIds, names, streaks, guesses, totalGames, gameNr]);
 
-	await db.query(serverQuery, [serverID]);
+		const serverQuery = `
+		INSERT INTO servers (server_id, initialized) VALUES($1, TRUE)
+		ON CONFLICT (server_id) DO UPDATE SET initialized = TRUE
+		`;
 
-	const userServerQuery = `
-	INSERT INTO user_servers (user_id, server_id) 
-	SELECT
-	    id,
-	    $2
-	FROM UNNEST($1::bigint[]) AS t(id)
-	ON CONFLICT (user_id, server_id) DO NOTHING
-	`;
+		await client.query(serverQuery, [serverID]);
 
-	await db.query(userServerQuery, [userIds, serverID]);
+		const userServerQuery = `
+		INSERT INTO user_servers (user_id, server_id) 
+		SELECT
+			id,
+			$2
+		FROM UNNEST($1::bigint[]) AS t(id)
+		ON CONFLICT (user_id, server_id) DO NOTHING
+		`;
+
+		await client.query(userServerQuery, [userIds, serverID]);
+
+		await client.query('COMMIT;');
+	}
+	catch (error) {
+		await client.query('ROLLBACK;');
+		console.log(error);
+	}
+	finally {
+		client.release();
+	}
+
 }
 
 async function updateTimeBuffer(userId, daytime, serverId) {
