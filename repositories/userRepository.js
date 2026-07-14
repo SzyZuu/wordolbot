10bb277a49f7c44aed673ab024a48b24cc7de01a
SzyZu <120480378+SzyZuu@users.noreply.github.com>
2026-07-14 02:10:03 +0200

    fix: avg calculation

--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -76,7 +76,7 @@ async function updateUser(userId, currentWordle) {
 			ELSE 1
 		END,
 		last_played = $2,
-		avg_guesses = ROUND((COALESCE(avg_guesses, 0) * games_played + (SELECT guesses FROM history WHERE wordle_number = $2 AND user_id = $1)) / games_played + 1),
+		avg_guesses = ROUND((COALESCE(avg_guesses, 0) * games_played + (SELECT guesses FROM history WHERE wordle_number = $2 AND user_id = $1)) / (games_played + 1)),
 		games_played = games_played + 1
 	WHERE user_id = $1
 	`;
