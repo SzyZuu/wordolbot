f021c2c32de317d08b0f78c23ebeac7fb790a948
SzyZu <120480378+SzyZuu@users.noreply.github.com>
2026-09-24 16:24:53 +0200

    Change command name from 'image' to 'stats'

--- commands/utility/image.js
+++ commands/utility/image.js
@@ -3,7 +3,7 @@ const { createImage } = require('../../helpers/imageGenerator');
 
 module.exports = {
     cooldown: 0,
-    data: new SlashCommandBuilder().setName('image').setDescription('debug'),
+    data: new SlashCommandBuilder().setName('stats').setDescription('View your wordle stats!'),
     async execute(interaction){
         const img = await createImage(interaction.user.displayAvatarURL({extension: 'jpg'}), interaction.member.displayName, interaction.user.id);
         const attachment = new AttachmentBuilder(img, { name: 'stat-banner.png' })
