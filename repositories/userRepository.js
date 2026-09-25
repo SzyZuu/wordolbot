5e8c0d576cbdb6ecd0d7a3a4e8d83e304ff2bfdd
SzyZu <skwarek116@gmail.com>
2026-09-25 14:18:56 +0200

    refactor: remove unused const

--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -1,5 +1,4 @@
 const db = require('../db');
-const {max} = require("pg/lib/defaults");
 
 async function initializeUsers(userIds, names, streaks, guesses, totalGames, gameNr, serverID) {
 	const client = await db.getClient();
