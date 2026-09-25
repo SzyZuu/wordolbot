3e70fa4c7bf94c2c2182e915f216c65b2ae7355a
SzyZu <skwarek116@gmail.com>
2026-09-25 16:44:02 +0200

    feat: choose target use for stats

--- commands/utility/stats.js
+++ commands/utility/stats.js
@@ -3,9 +3,16 @@ const { createImage } = require('../../helpers/imageGenerator');
 
 module.exports = {
     cooldown: 0,
-    data: new SlashCommandBuilder().setName('stats').setDescription('View your wordle stats!'),
+    data: new SlashCommandBuilder()
+        .setName('stats')
+        .setDescription('View your wordle stats!')
+        .addUserOption(option => option
+            .setName("target")
+            .setDescription("Target user to stalk :D")
+            .setRequired(false)),
     async execute(interaction){
-        const img = await createImage(interaction.user.displayAvatarURL({extension: 'jpg'}), interaction.member.displayName, interaction.user.id);
+        const target = interaction.options.getUser("target") ?? interaction.user;
+        const img = await createImage(target.displayAvatarURL({extension: 'jpg'}), target.displayName, target.id);
         const attachment = new AttachmentBuilder(img, { name: 'stat-banner.png' })
         interaction.reply({files: [attachment]})
     },
